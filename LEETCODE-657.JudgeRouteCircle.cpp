0048b022480d7c172fbae54dc8f1400c9add797e
sidhantchadha <sidhantchadha@live.com>
2018-03-31 17:56:15 -0500

    Leetcode solutions in C++

---
+++ LEETCODE-657.JudgeRouteCircle.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : JudgeRouteCircle.cpp
+// Author      : Sidhant Chadha
+// Version     :
+// Copyright   : 
+// Description : Judge Route Circle in C++, Ansi-style
+//============================================================================
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+     int v = 0;
+        int h = 0;
+        for (char ch : moves) {
+            switch (ch) {
+                case 'U' : v++;
+                    break;
+                case 'D' : v--;
+                    break;
+                case 'R' : h++;
+                    break;
+                case 'L' : h--;
+                    break;
+            }
+        }
+        return v == 0 && h == 0;
+    }
+};
