dc54026c94e596211198da25a63539b57d76a406
sidhantchadha <sidhantchadha@live.com>
2018-04-13 17:14:24 -0500

    Leetcode solutions in C++

---
+++ LEETCODE-136.SingleNumber.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : SingleNumber.cpp
+// Author      : Sidhant Chadha
+// Version     :
+// Copyright   : 
+// Description : Single Number in C++, Ansi-style
+//============================================================================
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        vector<int>count(256,0);
+        for(int i=0;i<nums.size();i++)
+            count[nums[i]]++;
+        for(int i=0;i<nums.size();i++)
+            if(count[nums[i]]==1)
+                return nums[i];
+    }
+};
