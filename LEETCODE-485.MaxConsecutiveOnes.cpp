0a0517b223d3503b0f5bb060804478ce50541475
sidhantchadha <sidhantchadha@live.com>
2018-04-17 14:36:25 -0500

    Leetcode solutions in C++

--- LEETCODE-485.MaxConsecutiveOnes.cpp
+++ LEETCODE-485.MaxConsecutiveOnes.cpp
@@ -1,47 +1,26 @@
 //============================================================================
-// Name        : LEETCODE-MaxConsecutiveOnes.cpp
+// Name        : MaxConsecutiveOnes.cpp
 // Author      : Sidhant Chadha
 // Version     :
 // Copyright   : 
-// Description : Maximum Consecutive Ones in C++, Ansi-style
+// Description : Max Consecutive Ones in C++, Ansi-style
 //============================================================================
-#include<iostream>
-#include<algorithm>
-#include<vector>
-using namespace std;
 
-int findMaxConsecutiveOnes(vector<int>& nums) {
-        int count=0;
-        vector<int>c;
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ctr=0;
+        int maxx=0;
+        vector<int>V;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1)
-                count++;
-            else if(nums[i]==0) {
-                c.push_back(count);
-                count=0;
-
+                ctr++;
+            if(nums[i]!=1 || i==nums.size()-1){
+                maxx=max(ctr,maxx);
+                ctr=0;
+            }
         }
-
+        return maxx;
     }
-        c.push_back(count);
-        make_heap(c.begin(),c.end());
-        return c.front();
+
 };
-
-int main() {
-	vector<int>v;
-	v.push_back(1);
-	v.push_back(0);
-	v.push_back(1);
-	v.push_back(1);
-
-	v.push_back(0);
-	v.push_back(0);
-	v.push_back(0);
-	v.push_back(1);
-	v.push_back(1);
-	v.push_back(1);
-
-cout<<findMaxConsecutiveOnes(v);
-
-}
