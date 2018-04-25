49f2698310a2a4bd33b00f1c81e8b7f247c56604
Sidhant Chadha <sidhantchadha@live.com>
2018-04-25 11:05:11 -0500

    Leetcode solutions in C++

--- LEETCODE-232.ImplementQueueUsingStack.cpp
+++ LEETCODE-232.ImplementQueueUsingStack.cpp
@@ -8,52 +8,51 @@
 
 class MyQueue {
 public:
-    stack<int>S;
-    stack<int>V;
+    stack<int>S1;
+    stack<int>S2;
     /** Initialize your data structure here. */
     MyQueue() {
-
+        
     }
-
+    
     /** Push element x to the back of queue. */
     void push(int x) {
-        S.push(x);
+        S1.push(x);
     }
-
+    
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        while(S.size()!=1) {
-            int temp=S.top();
-            V.push(temp);
-            S.pop();
+        while(S1.size()!=1){
+            S2.push(S1.top());
+            S1.pop();
         }
-        int val=S.top();
-        S.pop();
-        while(V.size()!=0) {
-            S.push(V.top());
-            V.pop();
+        int val=S1.top();
+        S1.pop();
+        while(!S2.empty()){
+            S1.push(S2.top());
+            S2.pop();
         }
         return val;
     }
-
+    
     /** Get the front element. */
     int peek() {
-          while(S.size()!=1) {
-            int temp=S.top();
-            V.push(temp);
-            S.pop();
+        while(S1.size()!=1){
+            S2.push(S1.top());
+            S1.pop();
         }
-        int val=S.top();
-        while(V.size()!=0) {
-            S.push(V.top());
-            V.pop();
+        int val=S1.top();
+        while(!S2.empty()){
+            S1.push(S2.top());
+            S2.pop();
         }
         return val;
+        
     }
-
+    
     /** Returns whether the queue is empty. */
     bool empty() {
-        return(!S.size());
+        return S1.empty();
     }
 };
 
