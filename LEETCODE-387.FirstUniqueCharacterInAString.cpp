e46cbf7f9e7cf0de64a81c4f5f03dc07a4415d14
Sidhant Chadha <sidhantchadha@live.com>
2018-05-01 13:25:54 -0500

    Leetcode solutions in C++

--- LEETCODE-387.FirstUniqueCharacterInAString.cpp
+++ LEETCODE-387.FirstUniqueCharacterInAString.cpp
@@ -3,13 +3,25 @@
 // Author      : Sidhant Chadha
 // Version     :
 // Copyright   : 
-// Description : Hello World in C++, Ansi-style
+// Description : First Unique Character In A String in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-using namespace std;
-
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
-}
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int>count(256,0);
+        int index=-1;
+        for(int i=0;i<s.length();i++){
+            count[s[i]]++;
+            
+        }
+        for(int i=0;i<s.length();i++){          
+        if(count[s[i]]==1){
+                index=i;
+                break;
+        }
+                }
+        return index;
+                
+    }
+};
