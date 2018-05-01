9d55d7bf2f7a78a197880f48fae56fd799a77d37
sidhantchadha <sidhantchadha@live.com>
2018-05-01 10:53:32 -0500

    Leetcode solutions in C++

---
+++ LEETCODE-3.LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : LongestSubstringWithoutRepeatingCharacters.cpp
+// Author      : Sidhant Chadha
+// Version     :
+// Copyright   : 
+// Description : Longest Substring Without Repeating Characters in C++, Ansi-style
+//============================================================================
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> count(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i< s.length(); i++) {
+            if (count[s[i]] > start)
+                start = count[s[i]];
+            count[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
