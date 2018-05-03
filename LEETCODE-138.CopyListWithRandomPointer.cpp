b475c6b8e63c3913c97f9a96de587b32ea9f85bc
sidhantchadha <sidhantchadha@live.com>
2018-05-03 13:27:41 -0500

    Leetcode solutions in C++

---
+++ LEETCODE-138.CopyListWithRandomPointer.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : CopyListWithRandomPointer.cpp
+// Author      : Sidhant Chadha
+// Version     :
+// Copyright   : 
+// Description : CopyListWithRandomPointer in C++, Ansi-style
+//============================================================================
+
+class Solution {
+public:
+    unordered_map<RandomListNode*, RandomListNode*>mp;
+    RandomListNode *copyRandomList(RandomListNode *head)
+    {
+        if(!head) return NULL;
+        if(mp[head]!=NULL) return mp[head];
+        mp[head] = new RandomListNode(head->label);
+        mp[head] -> next = copyRandomList(head->next);
+        mp[head] -> random = copyRandomList(head->random);
+        return mp[head];
+    }
+};
