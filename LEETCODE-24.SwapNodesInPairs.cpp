641ca1c4905e2afc9b8b638f4c3a3cd51f669122
sidhantchadha <sidhantchadha@live.com>
2018-05-04 10:22:45 -0500

    Leetcode solutions in C++

---
+++ LEETCODE-24.SwapNodesInPairs.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : SwapNodesInPairs.cpp
+// Author      : Sidhant Chadha
+// Version     :
+// Copyright   : 
+// Description : Swap Nodes In Pairs in C++, Ansi-style
+//============================================================================
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode *p=head;
+        if(p == NULL)
+            return NULL;
+        if(p->next == NULL)
+            return head;
+
+        ListNode* n = p->next;
+        p->next = swapPairs(n->next);
+        n->next = p;
+
+        return n;
+    }
+};
